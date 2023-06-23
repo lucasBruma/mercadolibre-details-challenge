6b2e762478e7287e7d23c940a381d24df6880c50
lucasBruma <lucasbrumatti99@gmail.com>
2023-06-23 14:19:30 -0300

    feat: agrego modal again

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {Container, Stack, Button, Text} from "@chakra-ui/react";
+import Swal from "sweetalert2";
 
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
@@ -7,6 +8,18 @@ import Footer from "../components/Footer";
 import Routes from "./routes";
 
 const App: React.FC = () => {
+  React.useEffect(() => {
+    Swal.fire({
+      title: "¡Atención!",
+      text:
+        "Esta web NO es el sitio oficial de Mercado Libre. Se trata de un challenge de Programación. NO es un intento de phishing hacia los usuarios que la visitan.",
+      icon: "warning",
+      confirmButtonText: "¡Entendido!",
+      confirmButtonColor: "#3484fa",
+      backdrop: "rgba(0, 0, 0, 0.8)",
+    });
+  }, []);
+
   return (
     <>
       <Navbar />
